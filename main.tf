123dae0dde7a6afb1167daa91391678547413424
shahin <shahin.ccie1989@gmail.com>
2023-01-30 23:10:55 +0100

    test cloud 2

--- main.tf
+++ main.tf
@@ -1,4 +1,8 @@
-provider "aws" {}
+provider "aws" {
+    access_key = var.aws_access_key_id.value
+    secret_key = var.aws_secret_access_key.value
+    region = var.aws_default_region.value
+}
 
 resource "aws_eip" "my_eip1" {
     vpc = true
